bb27c2d59c3e2be34fa16d42f1c45b6f1533c22d
Bartosz Janiak <janiak.bartosz92@gmail.com>
2020-06-14 13:39:51 +0200

    Add JS functions to display active article from link

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const titleClickHandler = function(event){
+    event.preventDefault();
     console.log('Link was clicked!');
+    const clickedElement = this;
     console.log(event);
   
     /* remove class 'active' from all article links  */
@@ -9,7 +11,9 @@ const titleClickHandler = function(event){
          activeLink.classList.remove('active'); 
     }
     /* add class 'active' to the clicked link */
-  
+    console.log('clickedElement:', clickedElement);
+    
+    clickedElement.classList.add('active');
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
@@ -17,10 +21,18 @@ const titleClickHandler = function(event){
       activeArticle.classList.remove('active');
     }
     /* get 'href' attribute from the clicked link */
+    const hrefClickedLinked = clickedElement.getAttribute('href');
+
+    console.log(hrefClickedLinked);
   
     /* find the correct article using the selector (value of 'href' attribute) */
-  
+    const correctArticles = document.querySelector(hrefClickedLinked);
+    //const correctArticles = document.getElementById(hrefClickedLinked);
+    // nie dziala by id bo widzi # z linka???
+    console.log(correctArticles);
+
     /* add class 'active' to the correct article */
+    correctArticles.classList.add('active');    
   }
   
   const links = document.querySelectorAll('.titles a');
